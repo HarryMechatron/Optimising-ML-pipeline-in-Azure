2602fe096705e8132674b03a59f5b3ba034e4042
HarryMechatron <64123552+HarryMechatron@users.noreply.github.com>
2021-04-14 07:04:31 +0000

    added train.py and project notebook

--- train.py
+++ train.py
@@ -9,21 +9,14 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Dataset , Datastore
+from azureml.data.datapath import DataPath
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###
-x_train, x_test, y_train, y_test = train_test_split(x, y)
-
-run = Run.get_context()
+ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv");
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,7 +43,16 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df
     
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train , x_test, y_train, y_test = train_test_split(x, y, train_size = 0.80, test_size = 0.20, random_state = 42)
+
+run = Run.get_context()
 
 def main():
     # Add arguments to script
@@ -67,7 +69,10 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    os.makedirs('outputs', exist_ok = "True")
+    joblib.dump(model,'outputs/model.joblib')
     run.log("Accuracy", np.float(accuracy))
+
 
 if __name__ == '__main__':
     main()
